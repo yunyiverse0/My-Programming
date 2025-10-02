f863d05ef1a7b1ac66691ee1085b35bd2f54a280
Yunyi Han <yunyiverse0302@gmail.com>
2025-10-03 08:31:34 +0900

    Create #9271.py

---
+++ python/Jungol/#9271.py
@@ -0,0 +1,25 @@
+# 정수 S와 정수 E를 입력받아 정수 S부터 정수 E까지의 모든 홀수를 출력하는 프로그램을 작성하시오. (S와 E는 홀수이며, S는 E보다 작거나 같음이 보장된다.)
+
+# 방법1
+S, E = map(int, input().split())
+
+for i in range(S, E+1, 2):
+    print(i)
+
+# if S와 E가 홀수라는 조건이 주워지지 않았을 때
+
+# 방법 1
+
+if S % 2 == 0: 
+    S += 1      # S가 홀수이면 시작한다는 조건 
+
+for i in range(S, E+1, 2):
+    print(i)
+
+# 방법2
+
+S, E = map(int, input().split())
+
+for i in range(S, E+1):
+    if i % 2 == 1:      # 조건문으로 i 나누가 2의 몫이 1일 때만 출력이 되도록 설정
+        print(i)
