9d99fb05c921742c28b32b86bff2b8aea382d73a
Yunyi Han <yunyiverse0302@gmail.com>
2025-10-16 19:10:02 +0900

    Create #A-all.py

---
+++ docs/Python/Jungol/02-변수와 입력/#A-all.py
@@ -0,0 +1,32 @@
+#######################################9195
+a = 'Chulsoo'; b = 100
+
+print('My name is',a,'\nMy score is',b)
+#######################################9196
+a = input()
+b = input()
+c = input()
+
+print(f'{c} graduated {a} {b} school.')
+
+#######################################9197
+a, b, c = map(int,input().split())
+
+print(f'{a} + {b} - {c} = {a+b-c}')
+
+#######################################9198
+a = float(input())
+b = float(input())
+
+print('total =', a + b)
+
+#######################################9199
+a,b = map(float,input().split())
+a = int(a)
+
+print(f'{a} + {b} =',a+b)
+
+#######################################9200
+y = float(input())
+
+print(f'{y} yard = {y*91.44} cm')
