93087a82d3aba660833351433ced378f9849aa2e
Yunyi Han <yunyiverse0302@gmail.com>
2025-10-18 15:02:11 +0900

    Update #A9371.py

--- docs/Python/Jungol/09.리스트1/#A9371.py
+++ docs/Python/Jungol/09.리스트1/#A9371.py
@@ -1,14 +1,14 @@
 # 첫 번째 줄에 두 개의 정수 a, b를 입력받아, a가 b개인 리스트를 만들고, 
 # 두 번째 줄에도 같은 작업을 반복하여 새로운 리스트를 만든 후, 두 리스트를 합하여 출력 예와 같이 출력하는 프로그램을 작성하시오.
 
+A = []; B = []
+
 a, b = map(int,input().split())
 c, d = map(int,input().split())
 
-A = [a, b]; B = [c, d]; C = []
-
 for i in range(b):
-    C.append(a)
+    A.append(a)
 for i in range(d):
-    C.append(c)
+    B.append(c)
 
-print(C)
+print(A+B)
