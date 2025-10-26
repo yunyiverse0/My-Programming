c3e59d7011b002bdb25ded713e4f7932ee2ca5e5
Yunyi Han <yunyiverse0302@gmail.com>
2025-10-27 08:11:05 +0900

    Update #9411.py

--- docs/Python/Jungol/11-리스트2/#9411.py
+++ docs/Python/Jungol/11-리스트2/#9411.py
@@ -1,3 +1,19 @@
+# 2행 3열의 2차원 리스트를 입력받아, 전체 리스트를 출력하고, 각 행 단위로 리스트를 출력하고, 각 원소를 2행 3열에 맞추어 출력하는 프로그램을 작성하시오.
+
+##### MY CODE
+
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+
+print([a,b])
+print(a)
+print(b)
+print(*a)
+print(*b)
+
+##### GPT CODE
+
 a = []
 
 for i in range(2):
