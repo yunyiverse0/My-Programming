555da3cc570dfe403c3fe655c089d40cf46384b6
Yunyi Han <yunyiverse0302@gmail.com>
2025-10-28 10:50:02 +0900

    Update step3.py

--- docs/Computing♡/W5-6/login_function_avator.py/step3.py
+++ docs/Computing♡/W5-6/login_function_avator.py/step3.py
@@ -13,5 +13,26 @@ def index():
         return redirect(url_for('welcome'))
     return redirect(url_for('login'))
 
+###########################################
+import json
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
+app = Flask(__name__)
+
+@app.route('/')     
+def index():            #index 함수 선언  (호출을 해야 실행 가능)    
+    if 'username' in session:
+        return redirect(url_for('welcome'))
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 # 2. 프로젝트 폴더 안에 test.py 폴더 생성
