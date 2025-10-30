88d57fb3a2635c7201581e1d4e29846c208dea64
Yunyi Han <yunyiverse0302@gmail.com>
2025-10-30 14:57:25 +0900

    Update #1-계단.py

--- docs/Python/NOTE/별찍기/#1-계단.py
+++ docs/Python/NOTE/별찍기/#1-계단.py
@@ -1,3 +1,4 @@
+#####계단
 N = int(input())
 
 for i in range(N):            # N = 5, i = 1,2,3,4
@@ -5,3 +6,23 @@ for i in range(N):            # N = 5, i = 1,2,3,4
   print('*' * N)
 
 
+#####삼각형
+# 왼쪽정렬 삼각형
+n = int(input())
+
+for i in range(1, n + 1):
+  print('*' * i)          #공백X
+  
+# 오른쪽정렬 삼각형
+
+n = int(input())
+
+for i in range(1, n + 1):
+  print(' ' * (n - i), end = '')
+  print('*' * i)
+
+# 역삼각형
+n = int(input())
+
+for i in range(N):
+  print('*' * (N - i))
