4c7f8e4c9e232fc686dc5b706569dde8ba6ff05d
Yunyi Han <yunyiverse0302@gmail.com>
2025-11-03 07:46:12 +0900

    Update #9474.py

--- docs/Python/Jungol/14-함수2/#9474.py
+++ docs/Python/Jungol/14-함수2/#9474.py
@@ -17,6 +17,21 @@ else:
 print(A)
 print(B)
 
+####################################2
+
+INT = list(map(int,input().split()))
+FLOAT = list(map(float,input().split()))
+
+if abs(INT[0]) > abs(INT[1]):
+    print(INT[0])
+else:
+    print(INT[1])
+
+if abs(FLOAT[0]) < abs(FLOAT[1]):
+    print(FLOAT[0])
+else:
+    print(FLOAT[1])
+
 ######GPT가 짜준 코드 
 
 a, b = map(int, input().split())
